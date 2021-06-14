2d4a6a4ac75ccf2b5e8f73c7be5ce151de6484fd
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-06-14 11:40:39 -0700

    feat/ create fectch utils

--- src/containers/Search/Search.jsx
+++ src/containers/Search/Search.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchComponent from '../../components/search/SearchComponent';
+import { fetchAllJobs } from '../../utils/searchUtils';
 
 const Search = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(async () => {
+    const fetchedJobs = await fetchAllJobs();
+    setJobs(fetchedJobs);
+  }, [jobs]);
+
   return (
     <div>
       <SearchComponent />
